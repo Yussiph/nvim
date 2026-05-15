1df77b2871dff339fca5a212fbabf88d0a7955d1
Yussif <yussifsalahebrahim@gmail.com>
2026-05-16 01:02:23 +0300

    some 'improvements'

--- init.lua
+++ init.lua
@@ -24,6 +24,11 @@ vim.pack.add({
 	'https://github.com/nvim-mini/mini.files',
 	'https://github.com/echasnovski/mini.indentscope',
 	'https://github.com/echasnovski/mini.diff',
+	'https://github.com/echasnovski/mini.statusline',
+	'https://github.com/echasnovski/mini.cursorword',
+	'https://github.com/echasnovski/mini.icons',
+	'https://github.com/folke/tokyonight.nvim',
+	'https://github.com/wurli/cobalt.nvim',
 })
 
 require('mini.pairs').setup({})
@@ -53,15 +58,23 @@ require('mini.indentscope').setup({
 require('mini.diff').setup({
 	view = {
 		style = 'sign',
+		signs = { add = '+', change = '~', delete = '-'},
 	},
 })
 
-require('catppuccin').setup({
-	flavour = "mocha",
-	transparent_background = true,
+require('mini.statusline').setup({
+	set_vim_settings = true,
 })
 
-vim.cmd.colorscheme 'catppuccin'
+require('mini.cursorword').setup({})
+
+require('mini.icons').setup({
+	style = 'glyph',
+})
+MiniIcons.mock_nvim_web_devicons()
+
+
+
 
 -- Stop auto-commenting new lines
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -71,5 +84,130 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 
+-- == Flutter Setup ==
+-- Fetch the official LSP configuration and Flutter tools ecosystem cleanly using vim.pack
+vim.pack.add({
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/nvim-flutter/flutter-tools.nvim' }
+})
+
+-- Set up Flutter tools (it automatically sets up dartls behind the scenes)
+require('flutter-tools').setup({
+  ui = {
+    border = 'rounded', -- Clean rounded panels for floating documentation
+  },
+  decorations = {
+    statusline = {
+      device = true,    -- Shows your current active mobile device/emulator in your statusline
+      app_version = true,
+    }
+  },
+  lsp = {
+    -- The deprecated color configuration has been removed from here!
+
+    -- Attach our global keyboard-centric keymaps and settings when the server connects
+    on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr, silent = true }
+
+      -- Enable modern native Neovim color highlights for Flutter colors/icons
+      if vim.lsp.document_color and vim.lsp.document_color.enable then
+        vim.lsp.document_color.enable(true, { bufnr = bufnr })
+      end
+
+      -- Keymaps for navigating and auditing code structures
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)      -- Go to definition
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)            -- Show documentation overview
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts) -- Code actions
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)   -- Rename symbol across project
+    end,
+  }
+})
+
+-- Update your plugin list to pull in blink
+vim.pack.add({
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-flutter/flutter-tools.nvim',
+  'https://github.com/saghen/blink.cmp', -- Add the completion engine
+})
+
+-- Configure Blink for automated neon-vivid popup listings
+require('blink.cmp').setup({
+  keymap = {
+    preset = 'default', -- Sets up Up/Down arrows or Ctrl-n/Ctrl-p out of the box
+    ['<Tab>'] = { 'select_next', 'fallback' },
+    ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    ['<CR>'] = { 'accept', 'fallback' }, -- Enter key accepts the suggestion
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+})
+
+ -- Themes --
+
+require("tokyonight").setup({
+	style = 'moon',
+	transparent = true,
+	styles = {
+		sidebars = trasparent,
+		floats = trasparent,
+	},
+	on_colors = function(colors)
+		colors.git.add = '#44dfaa'
+		colors.git.change = '#3daee9'
+		colors.git.delete = '#ff5370'
+	end,
+})
 
 
+
+local colors = {
+  'Normal', 'NormalFloat', 'SignColumn', 'LineNr', 
+  'FoldColumn', 'StatusLine', 'StatusLineNC', 'WinSeparator'
+}
+
+for _, group in ipairs(colors) do
+  vim.api.nvim_set_hl(0, group, { bg = 'NONE', ctermbg = 'NONE' })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
+})
+
+require('catppuccin').setup({
+	-- flavour = "mocha",
+	transparent_background = true,
+})
+
+
+-- vim.cmd.colorscheme 'cobalt2'
+vim.cmd[[colorscheme tokyonight]]
+
+vim.api.nvim_set_hl(0, "MiniCursorword", { 
+  underline = true, 
+  bg = "NONE" 
+})
+
+vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { 
+  underline = true, 
+  bg = "NONE" 
+})
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch', -- Uses your theme's vibrant search highlight color
+      timeout = 150,         -- How long the flash lasts in milliseconds
+    })
+  end,
+})
+
+-- Clear search highlights on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
